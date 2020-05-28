84223f500ee909627d08be1db9d209b662006f65
BH3Durandal <65000673+BH3Durandal@users.noreply.github.com>
2020-05-28 14:23:58 +0800

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -23,7 +23,7 @@ def index(request):
     allarticle = Article.objects.all().order_by('-id')[0:10]
     hot = Article.objects.all().order_by('views')[:10]
     link = Link.objects.all()
-    return render(request, 'article.html', locals())
+    return render(request, 'index.html', locals())
 
 #列表页
 def list(request,lid):
